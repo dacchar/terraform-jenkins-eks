95c9f7edc8a37ee66ae43e8e606f7fc72f63f4a5
Dmitry Charushnikov <Dmitry.Charushnikov@msg.group>
2025-03-06 15:53:24 +0100

    sync

--- Jenkins Server/main.tf
+++ Jenkins Server/main.tf
@@ -68,12 +68,12 @@ module "ec2_instance" {
   name = "Jenkins-Server"
 
   instance_type               = var.instance_type
-  key_name                    = "jenkins-server-key"
+  key_name                    = "asecgurukey"
   monitoring                  = true
   vpc_security_group_ids      = [module.sg.security_group_id]
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  user_data                   = file("jenkins-install.sh")
+  //user_data                   = file("jenkins-install.sh")
   availability_zone           = data.aws_availability_zones.azs.names[0]
 
   tags = {
